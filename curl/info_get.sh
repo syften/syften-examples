3a80cd5b42f46bfaeffbb46bfbdc5620a898f59d
Michal Mazurek <akfaew@gmail.com>
2019-07-18 10:04:56 +0200

    Rename the api 0.0 endpoint from stats to info

--- curl/info_get.sh
+++ curl/info_get.sh
@@ -5,5 +5,5 @@
 # MENTIONME_APITOKEN= # Your API Token from https://mentionme.app/setup
 MENTIONME_APITOKEN=${MENTIONME_APITOKEN:?You must provide your API Token}
 
-curl -X POST -H "Authorization: Bearer $MENTIONME_APITOKEN" -H "Content-length: 0" https://mentionme.app/api/0.0/stats/get
+curl -X POST -H "Authorization: Bearer $MENTIONME_APITOKEN" -H "Content-length: 0" https://mentionme.app/api/0.0/info/get
 echo # curl doesn't produce a newline
