16fe7f837e850a7eacccaeb7d795b2e624c0839a
Michal Mazurek <akfaew@gmail.com>
2019-09-20 09:10:17 +0200

    Fix url

--- curl/items_get.sh
+++ curl/items_get.sh
@@ -2,8 +2,8 @@
 #
 # Communicate with the API using cURL.
 
-# SYFTEN_APITOKEN= # Your API Token from https://syften/setup
+# SYFTEN_APITOKEN= # Your API Token from https://syften.com/setup
 SYFTEN_APITOKEN=${SYFTEN_APITOKEN:?You must provide your API Token}
 
-curl -X POST -H "Authorization: Bearer $SYFTEN_APITOKEN" -H "Content-length: 0" https://syften/api/0.0/items/get
+curl -X POST -H "Authorization: Bearer $SYFTEN_APITOKEN" -H "Content-length: 0" https://syften.com/api/0.0/items/get
 echo # curl doesn't produce a newline
