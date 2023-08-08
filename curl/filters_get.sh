06281e3ed089532f48f6d6c7f8890799758bcb21
Michal Mazurek <akfaew@gmail.com>
2023-08-08 08:56:16 +0200

    Add filters/get and filters/set

---
+++ curl/filters_get.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Communicate with the API using cURL.
+
+# SYFTEN_APITOKEN= # Your API Token from https://syften.com/setup
+SYFTEN_APITOKEN=${SYFTEN_APITOKEN:?You must provide your API Token}
+
+curl -X POST -H "Authorization: Bearer $SYFTEN_APITOKEN" -H "Content-length: 0" https://syften.com/api/0.0/filters/get
+echo # curl doesn't produce a newline
---
+++ curl/filters_set.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Communicate with the API using cURL.
+
+# SYFTEN_APITOKEN= # Your API Token from https://syften.com/setup
+SYFTEN_APITOKEN=${SYFTEN_APITOKEN:?You must provide your API Token}
+
+curl -X POST -H "Authorization: Bearer $SYFTEN_APITOKEN" \
+	  -H "Content-Type: application/json" \
+	  -d '{"filters": ["aaaaaaaaa", "bbbbbbbbb", "ccccccccc"]}' \
+	  https://syften.com/api/0.0/filters/set
+echo # curl doesn't produce a newline
