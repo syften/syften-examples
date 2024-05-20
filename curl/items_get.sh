0d98e94c7e79f5e6ac4cb3f911087a3c1e719cc1
Michal Mazurek <akfaew@gmail.com>
2024-05-20 09:56:05 +0200

    Limit items to 25

--- curl/items_get.sh
+++ curl/items_get.sh
@@ -4,5 +4,9 @@
 # SYFTEN_APITOKEN= # Your API Token from https://syften.com/setup
 SYFTEN_APITOKEN=${SYFTEN_APITOKEN:?You must provide your API Token}
 
+# Get all
 curl -X POST -H "Authorization: Bearer $SYFTEN_APITOKEN" -H "Content-length: 0" https://syften.com/api/0.0/items/get
+
+# Get only 25
+curl -X POST -H "Authorization: Bearer $SYFTEN_APITOKEN" -H "Content-Type: application/json"  -d '{"limit": 25}' https://syften.com/api/0.0/items/get
 echo
